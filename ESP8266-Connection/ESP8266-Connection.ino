f75994e382c35a5f3b367438734644769436bc10
pintarg <pintar.gasper@gmail.com>
2016-08-30 22:28:36 +0200

    Urejanje .ino kode, za delovanje 3 gumbov in 3 LEDic

--- ESP8266-Connection/ESP8266-Connection.ino
+++ ESP8266-Connection/ESP8266-Connection.ino
@@ -1,5 +1,5 @@
 // Opis: Koda, ki se izvaja na ESP8266 in skrbi za vzpostavitev WiFi povezave z routerjem, zajem glasov in komunikacijo s spletno stranjo.
-// Verzija: 2016.08.24b
+// Verzija: 2016.08.30a
 // ====================================================================================================
 
 // 470 ohm
@@ -30,16 +30,21 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
 
         case WStype_TEXT:
         {
-          if (digitalRead(13) == 0) { // D7-ZE
-            Signal = 5;
-          } else if (digitalRead(12) == 0) { // D6-RU
-            Signal = 4;
-          } else if (digitalRead(0) == 0) { // D3-BE
+          if (digitalRead(0) == 0) { // D3-proti (13)
             Signal = 3;
-          } else if (digitalRead(4) == 0) { // D2-OR
+            digitalWrite(13, 1);
+            delay(1000);
+            digitalWrite(13, 0);
+          } else if (digitalRead(4) == 0) { // D2-nedolocen (12)
             Signal = 2;
-          } else if (digitalRead(5) == 0) { // D1-RD
+            digitalWrite(12, 1);
+            delay(1000);
+            digitalWrite(12, 0);
+          } else if (digitalRead(5) == 0) { // D1-za (14)
             Signal = 1;
+            digitalWrite(14, 1);
+            delay(1000);
+            digitalWrite(14, 0);
           } else {
             Signal = 0;
           }
@@ -63,11 +68,12 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
 }
 
 void setup() {
-    pinMode(13, INPUT_PULLUP); // D7 zelena
-    pinMode(12, INPUT_PULLUP); // D6 rumena
-    pinMode(0, INPUT_PULLUP); // D3 bela
-    pinMode(4, INPUT_PULLUP); // D2 oranžna
-    pinMode(5, INPUT_PULLUP); // D1 rdeča
+    pinMode(0, INPUT_PULLUP); // D3
+    pinMode(4, INPUT_PULLUP); // D2
+    pinMode(5, INPUT_PULLUP); // D1
+    pinMode(12, OUTPUT); // D6
+    pinMode(13, OUTPUT); // D7
+    pinMode(14, OUTPUT); // D4
 
     Serial.begin(115200);
     WiFi.begin(ssid, password);
