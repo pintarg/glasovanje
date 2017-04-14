7fb035287e766b8d161a85b1fc20d002ed5b1808
pintarg <pintar.gasper@gmail.com>
2017-04-14 11:49:51 +0200

    2017.04.14

--- glasovanje/verzije/server_2017.04.14a.js
+++ glasovanje/verzije/server_2017.04.14a.js
@@ -1,4 +1,4 @@
-// Verzija: 2017.04.14b
+// Verzija: 2017.04.14a
 // ====================================================================================================
 var express = require("express")();
 var http = require("http").Server(express);
