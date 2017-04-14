f6ad51aede696e6f2206fd982cc34cd1984ad4dc
pintarg <pintar.gasper@gmail.com>
2017-04-14 14:52:35 +0200

    2017.04.14

--- glasovanje/verzije/app_2017.04.14b.js
+++ glasovanje/verzije/app_2017.04.14b.js
@@ -1,4 +1,4 @@
-// Verzija: 2017.04.14c
+// Verzija: 2017.04.14b
 // ====================================================================================================
 var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'smart-table']);
 var removeRowPodatek, removeRowVprasanje;
